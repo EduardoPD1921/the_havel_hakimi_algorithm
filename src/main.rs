301b1d85ebed16f57eb03a601e71fa3f5c6da725
Eduardo <eduardo.gheleno@gmail.com>
2021-03-30 21:26:54 -0300

    Adicionando as primeiras funçoes remove_all_zeros e descending_sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::mem::replace;
+
+fn remove_all_zeros(vec: &mut Vec<i32>) {
+    vec.retain(|&x| x != 0);
+}
+
+fn descending_sort(vec: &mut Vec<i32>) {
+    vec.sort_by(|a, b| b.cmp(a));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut v = vec![6, 7, 1, 9];
+
+    remove_all_zeros(&mut v);
+
+    println!("{:?}", v);
+
+    descending_sort(&mut v);
+
+    println!("{:?}", v);
 }
